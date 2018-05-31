c7aaa1d91266b2dfab39c61d9cc4e7ea9d7e362f
schew2 <piggychew96@hotmail.com>
2018-05-31 14:51:18 -0500

    first commit

---
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
